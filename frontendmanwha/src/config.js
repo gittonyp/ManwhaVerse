bbafda55de0ea6f977c51795e558f069c11299e4
Tony <vqvtony@gmail.com>
2026-05-16 10:55:12 +0530

    render

--- frontendmanwha/src/config.js
+++ frontendmanwha/src/config.js
@@ -1,3 +1,3 @@
 // src/config.js
-export const API_BASE_URL = "http://localhost:8081/api";
-export const STATIC_BASE_URL = "http://localhost:8081";
+export const API_BASE_URL = "https://manwhaverse-docker-hosted.onrender.com/api";
+export const STATIC_BASE_URL = "https://manwhaverse-docker-hosted.onrender.com/";
